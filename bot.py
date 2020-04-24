b1aa42e5fac33d8bfa7ff2aec1c541c473f891a4
TechnicPlay <33784577+TechnicPlay@users.noreply.github.com>
2020-04-24 11:44:57 +0200

    first commands

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ from tf_logs import get_player_log_lists, get_latest_log_descr, get_log, summari
 from log_user import LogUser
 
 
+CMD_PREFIX = "!"
+
+
 class LogClient(discord.Client):
     def __init__(self, token):
         super(LogClient, self).__init__()
@@ -17,25 +20,26 @@ class LogClient(discord.Client):
         super(LogClient, self).run(self.token)
 
 
-    def get_subscribed_user(self, discord_user, steam_id_64):
+    def get_subscribed_user(self, discord_user_id=None, steam_id_64=None):
         for u in self.subscribed_users:
-            if u.contains(discord_user, steam_id_64):
+            if u.contains(discord_user_id, steam_id_64):
                 return u
         return None
 
 
-    def subscribe_user(self, discord_user, steam_id_64):
-        if self.get_subscribed_user(discord_user, steam_id_64) == None:
-            return False
+    def subscribe_user(self, discord_user_id, steam_id_64):
+        u = self.get_subscribed_user(discord_user_id, steam_id_64)
+        if u != None:
+            return u
 
-        u = LogUser(discord_user, steam_id_64)
+        u = LogUser(discord_user_id, steam_id_64)
         self.subscribed_users.append(u)
 
-        return True
+        return u
 
 
-    def unsubscribe_user(self, discord_user, steam_id_64):
-        u = self.get_subscribed_user(discord_user, steam_id_64)
+    def unsubscribe_user(self, discord_user_id=None, steam_id_64=None):
+        u = self.get_subscribed_user(discord_user_id, steam_id_64)
 
         if u == None:
             return False
@@ -46,6 +50,7 @@ class LogClient(discord.Client):
 
     async def log_update_loop(self):
         while True:
+
             await asyncio.sleep(1)
             
 
@@ -57,5 +62,25 @@ class LogClient(discord.Client):
 
 
     async def on_message(self, message):
+        if message.author == self.user:
+            return
+
         print('Message from {0.author}: {0.content}'.format(message))
+
+        if message.content[0] == CMD_PREFIX:
+            #dm only
+            if message.channel.id == message.author.dm_channel.id:
+                if "subscribe" in message.content:
+                    res = re.findall("[0-9]{17}", message.content)
+                    if len(res) == 1:
+                        steam_id = res[0]
+                        u = self.subscribe_user(message.author.id, steam_id)
+                    else:
+                        print("Wrong Cmd Format!")
+
+                    
+
+                    
+
+
 
--- log_user.py
+++ log_user.py
@@ -1,18 +1,23 @@
+import discord
+import asyncio
 from typing import overload
 
 class LogUser:
-    def __init__(self, discord_user, steam_id_64):
-        self.discord_user = discord_user
+    def __init__(self, discord_user_id, steam_id_64):
+        self.discord_user_id = discord_user_id
         self.steam_id_64 = steam_id_64
 
 
     def is_online(self):
         pass
     
+    
+    async def fetch_user(self, client):
+        return await client.fetch_user(self.discord_user_id)
 
-    def contains(self, discord_user, steam_id_64):
-        return steam_id_64 == self.steam_id_64 and discord_user == self.discord_user
+    def contains(self, discord_user_id, steam_id_64):
+        return steam_id_64 == self.steam_id_64 or discord_user_id == self.discord_user_id
 
 
     def equals(self, user):
-        return self.contains(user.discord_user, user.steam_id_64)
+        return user.steam_id_64 == self.steam_id_64 and user.discord_user_id == self.discord_user_id
